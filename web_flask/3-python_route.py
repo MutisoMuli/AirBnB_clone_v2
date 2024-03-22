e5ebfd2762b9789eceb50b576178b928d004a300
MutisoMuli <mulimutiso525@gmail.com>
2024-03-22 21:49:07 +0300

    Done

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,20 +7,24 @@ from flask import Flask
 
 app = Flask("__name__")
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Return a given string"""
     return ("Hello HBNB!")
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Return a given string"""
     return ("HBNB")
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def cText():
     """Display C followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
